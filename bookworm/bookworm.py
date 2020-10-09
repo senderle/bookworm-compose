368f3ef6d925490ee63f1fbd11045492d2639ebe
Scott Enderle <scott.enderle@gmail.com>
2020-10-09 13:45:41 +0000

    first working version

--- bookworm/bookworm.py
+++ bookworm/bookworm.py
@@ -1,6 +1,14 @@
-import pandas
-from time import sleep
+from flask import Flask
+app = Flask(__name__)
 
-while True:
-    print(pandas.__version__)
-    sleep(5)
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/cgi-bin')
+def hello_cgi():
+    return "Hello Cgi-Bin!"
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
