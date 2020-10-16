bb0f44ceba8ee5852010ea6ae6fc1ea19db21ed8
Scott Enderle <scott.enderle@gmail.com>
2020-10-16 13:09:34 -0400

    use docker secrets

--- bookworm/setup_mysql_passwords.sh
+++ bookworm/setup_mysql_passwords.sh
@@ -5,16 +5,15 @@ mkdir -p /etc/bookworm
 # ALways overwrite the admin settings.
 echo "[client]" > /etc/bookworm/admin.cnf
 echo "user=root" >> /etc/bookworm/admin.cnf
-echo "password=$MYSQL_ROOT_PASSWORD" >> /etc/bookworm/admin.cnf
+echo "password=`cat $MYSQL_ROOT_PASSWORD_FILE`" >> /etc/bookworm/admin.cnf
 echo "host=mariadb" >> /etc/bookworm/admin.cnf
 echo "clienthostname=" >> /etc/bookworm/admin.cnf
 
 # The client password should only be generated once.
 if [ ! -f "/etc/bookworm/client.cnf" ]; then
-  RAND=$(openssl rand -base64 20)
   echo "[client]" > /etc/bookworm/client.cnf
   echo "user=bookworm" >> /etc/bookworm/client.cnf
-  echo "password=$RAND" >> /etc/bookworm/client.cnf
+  echo "password=`cat $MYSQL_PASSWORD_FILE`" >> /etc/bookworm/client.cnf
   echo "host=mariadb" >> /etc/bookworm/client.cnf
   echo "clienthostname=" >> /etc/bookworm/client.cnf
 fi
